8ac0226286e8ef060454da2763b04bdacc450eba
Muhsin Ali <muhsin.ali@student.moringaschool.com>
2024-09-29 12:26:49 +0300

    Adds index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+// Function expression called divide
+const divide = (a, b) => a / b;
+
+// Arrow function called square
+const square = x => x * x;
+
+// Arrow function called add
+const add = (a, b) => a + b;
+
+// Test suite
+describe("index.js", () => {
+  it("has a function expression called divide", () => {
+    expect(divide).to.exist;
+  });
+
+  it("divide divides 2000 by 100", () => {
+    let a = 2000;
+    let b = 100;
+    expect(divide(a, b)).to.equal(a / b);
+  });
+
+  it("has an arrow function called square", () => {
+    expect(square).to.exist;
+  });
+
+  it("square arrow function takes one parameter and multiplies it times itself", () => {
+    let x = 2;
+    expect(square(x)).to.equal(4);
+  });
+
+  it("has an arrow function called add", () => {
+    expect(add).to.exist;
+  });
+
+  it("add arrow function takes two parameters and adds them together", () => {
+    let a = 3;
+    let b = 4;
+    expect(add(a, b)).to.equal(a + b);
+  });
+});
